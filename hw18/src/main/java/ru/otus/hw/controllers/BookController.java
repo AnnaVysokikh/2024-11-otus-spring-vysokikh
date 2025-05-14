886f842f93f0afe0310e9a513f2cdc3555c27d87
VysokikhAnna <anutka320@gmail.com>
2025-05-14 16:05:47 +0300

    hw18

--- hw18/src/main/java/ru/otus/hw/controllers/BookController.java
+++ hw18/src/main/java/ru/otus/hw/controllers/BookController.java
@@ -1,7 +1,10 @@
 package ru.otus.hw.controllers;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,16 +17,21 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import ru.otus.hw.dto.BookDto;
 import ru.otus.hw.dto.ShortBookDto;
+import ru.otus.hw.exceptions.TooManyRequestsException;
 import ru.otus.hw.services.BookService;
 
+import java.util.Collections;
 import java.util.List;
 
 @RequiredArgsConstructor
+@Slf4j
 @RestController
 @RequestMapping("/books")
 public class BookController {
     private final BookService bookService;
 
+    @RateLimiter(name = "rateLimiter", fallbackMethod = "rateLimiterFallback")
+    @CircuitBreaker(name = "circuitBreaker", fallbackMethod = "fallback")
     @GetMapping
     public List<BookDto> getAll() {
         return bookService.findAll();
@@ -49,5 +57,15 @@ public class BookController {
     @DeleteMapping("/{id}")
     public void delete(@PathVariable("id") long id) {
         bookService.deleteById(id);
+    }
+
+    private List<BookDto> fallback(Exception e) {
+        log.error("Отработал Circuit Breaker: {}", e.getMessage());
+        return Collections.emptyList();
+    }
+
+    private List<BookDto> rateLimiterFallback(Exception e) {
+        log.warn("Превышен лимит запросов: {}", e.getMessage());
+        throw new TooManyRequestsException("Превышен лимит запросов. Попробуйте позже");
     }
 }
